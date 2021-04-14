c7a320022b27ecdbacc75025da1863b6fb1e9a38
zeuspod <josseluism@gmail.com>
2021-04-13 22:03:39 -0400

    se corrigio la conexion con Json y se agrego un loop para actualizar los precios

--- main.py
+++ main.py
@@ -1,40 +1,43 @@
+import threading
 import discord
+from discord import embeds
 from discord.colour import Color
 from discord.ext import commands
+from discord.utils import async_all
 from pycoingecko import CoinGeckoAPI
 import json
 import datetime
 import time 
-
-cg = CoinGeckoAPI()
-#get price for coins from api
-price_bitcoin = []
-price_bitcoin = cg.get_price(ids=["Bitcoin", "ethereum", "Dogecoin"], vs_currencies="usd")
-
-#Save the data in a Json file 
-with open ('data.json' ,'w') as file:
-    json.dump(price_bitcoin, file, indent = 3)
-
-#Load data from Json file
-coinsfile = open ('data.json', 'r') 
-jsondata = coinsfile.read()
-#parser
-obj = json.loads(jsondata)
-
-#btc price 
-btc = obj['bitcoin']
-btcprice = btc['usd'] 
-
-#ethereum price 
-eth = obj['ethereum']
-ethprice = eth['usd'] 
-
-#dogecoin price 
-doge = obj['dogecoin']
-dogeprice = doge['usd'] 
+from threading import Timer
 
 
 
+
+def getprice():
+    cg = CoinGeckoAPI()
+    coinsprice = cg.get_price(ids='bitcoin, ethereum, dogecoin' , vs_currencies='usd')
+    print(coinsprice)
+    #Save the data in a Json file 
+    with open ('data.json' ,'w') as file:
+        json.dump(coinsprice, file, indent = 3)
+
+    #Load data from Json file
+    coinsfile = open ('data.json', 'r') 
+    jsondata = coinsfile.read()
+    #parser
+    obj = json.loads(jsondata)
+    threading.Timer(10,getprice).start()
+
+
+def printtest():
+    precios = getprice()
+    print(precios)
+    threading.Timer(8,printtest)
+
+imprime = printtest()
+
+
+#Discord bot 
 
 bot = commands.Bot(command_prefix='>')
 
@@ -43,32 +46,63 @@ bot = commands.Bot(command_prefix='>')
 async def on_ready():
     print('Bot activo')
 
+@bot.command()
+async def comandos(ctx):
+    embed = discord.Embed(title = f"{ctx.guild.name}", description="comandos para consultar las cotizaciones", timestamp=datetime.
+    datetime.utcnow(), Color=discord.Color.blue())
+    embed.add_field(name="Consulta BTC", value=">preciobtc")
+    embed.add_field(name="Consulta ETH", value=">precioeth")
+    embed.add_field(name="Consulta DOG", value=">preciodoge")
+    await ctx.send(embed=embed)
+
 #command BTC price
 @bot.command()
 async def preciobtc(ctx):
-       embed = discord.Embed(title = f"{ctx.guild.name}", description=f"**Precio del BTC {btcprice}**", timestamp=datetime.datetime.utcnow(), Color=discord.Color.blue())
-       embed.add_field(name="Price Font", value="Powered by Coingeicko")
+       #read json file 
+       coinsfile = open ('data.json', 'r') 
+       jsondata = coinsfile.read()
+       #parser
+       obj = json.loads(jsondata)
+       #btc price from jason
+       btc = obj['bitcoin']
+       precioBtc = btc['usd']
+       embed = discord.Embed(title = f"{ctx.guild.name}", description=f"**Precio del BTC {precioBtc}**", timestamp=datetime.datetime.utcnow(), Color=discord.Color.blue())
+       embed.add_field(name="Fuente de precio", value="Powered by Coingecko")
        embed.add_field(name="Dev", value="Jose Morales")
        embed.set_thumbnail(url="https://assets.coingecko.com/coins/images/1/small/bitcoin.png?1547033579")
        await ctx.send(embed=embed)
 
-#command ethereum price 
+#ethreum price
 @bot.command()
 async def precioeth(ctx):
-       embed = discord.Embed(title = f"{ctx.guild.name}", description=f"**Precio del ETH {ethprice}**", timestamp=datetime.datetime.utcnow(), Color=discord.Color.blue())
-       embed.add_field(name="Price Font", value="Powered by Coingeicko")
+       #read json file 
+       coinsfile = open ('data.json', 'r') 
+       jsondata = coinsfile.read()
+       #parser
+       obj = json.loads(jsondata)
+       #btc price from jason
+       eth = obj['ethereum']
+       ethprice = eth['usd']
+       embed = discord.Embed(title = f"{ctx.guild.name}", description=f"**Precio del Ethereum {ethprice}**", timestamp=datetime.datetime.utcnow(), Color=discord.Color.blue())
+       embed.add_field(name="Fuente de precio", value="Powered by Coingecko")
        embed.add_field(name="Dev", value="Jose Morales")
        embed.set_thumbnail(url="https://assets.coingecko.com/coins/images/279/small/ethereum.png?1595348880")
        await ctx.send(embed=embed)
 
-#command dogecoin price 
 @bot.command()
-async def preciodogecoin(ctx):
-       embed = discord.Embed(title = f"{ctx.guild.name}", description=f"**Precio de Dogecoin {dogeprice}**", timestamp=datetime.datetime.utcnow(), Color=discord.Color.blue())
-       embed.add_field(name="Price Font", value="Powered by Coingeicko")
+async def preciodoge(ctx):
+       #read json file 
+       coinsfile = open ('data.json', 'r') 
+       jsondata = coinsfile.read()
+       #parser
+       obj = json.loads(jsondata)
+       #btc price from jason
+       doge = obj['dogecoin']
+       dogeprice = doge['usd']
+       embed = discord.Embed(title = f"{ctx.guild.name}", description=f"**Precio del Dogecoin {dogeprice}**", timestamp=datetime.datetime.utcnow(), Color=discord.Color.blue())
+       embed.add_field(name="Fuente de precio", value="Powered by Coingecko")
        embed.add_field(name="Dev", value="Jose Morales")
        embed.set_thumbnail(url="https://assets.coingecko.com/coins/images/5/small/dogecoin.png?1547792256")
        await ctx.send(embed=embed)
-
 
 bot.run('ODMxMTg2MjQ3OTI0ODQyNTI3.YHRkhA.jf-u-GFzImrYlD8o-yzF6IJD7sU')
